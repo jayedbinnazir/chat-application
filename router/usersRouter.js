a4a559cc90abc0f17b80cc8b6ec870d8175cb2ed
jayedpr <jayed.freelance@gmail.com>
2023-12-18 16:40:29 +0600

    Role Authoriazation completed

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,14 +3,14 @@ const { getUsers, addUser, deleteUser } = require('../controller/usersController
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse');
 const avatarUpload = require('../middlewares/users/avatarUpload');
 const { addValidators, addValidationHandler } = require('../middlewares/users/userValidators');
-const { checkLogin } = require('../middlewares/common/checkLogin');
+const { checkLogin, requireRole } = require('../middlewares/common/checkLogin');
 
 const router = express.Router();
 const page_title = "User"
 
 
 
-router.get('/', decorateHtmlResponse(page_title) , checkLogin  , getUsers )
+router.get('/', decorateHtmlResponse(page_title) , checkLogin  , requireRole(['admin'])  , getUsers )
 
 //create users
 router.post('/' ,avatarUpload , addValidators , addValidationHandler , addUser  )
